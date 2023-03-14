3b30f06cdad86ea534435c129b3fa0d06140dde5
Juande Delgado Bermúdez <25461819+JD0001@users.noreply.github.com>
2023-03-14 18:33:32 +0100

    Change 'USA' to 'US'

--- src/app/shared/mocks/db.js
+++ src/app/shared/mocks/db.js
@@ -1,11 +1,11 @@
 const heroes = [
-	{id:1, alias: 'Captain America', name: 'Steve Rogers', country: 'USA'},
+	{id:1, alias: 'Captain America', name: 'Steve Rogers', country: 'US'},
 	{id:2, alias: 'Black widow', name: 'Natasha Romanova', country: 'RU'},
-	{id:3, alias: 'Iron Man', name: 'Anthony Stark', country: 'USA'},
+	{id:3, alias: 'Iron Man', name: 'Anthony Stark', country: 'US'},
 	{id:4, alias: 'Hulk', name: 'Bruce Banner', country: 'BR'},
-	{id:5, alias: 'Hawkeye', name: 'Clint Barton', country: 'USA'},
+	{id:5, alias: 'Hawkeye', name: 'Clint Barton', country: 'US'},
 	{id:6, alias: 'Black Phanter', name: 'T\'Challa', country: 'WA'},
-	{id:7, alias: 'Spiderman', name: 'Peter Parker', country: 'USA'}
+	{id:7, alias: 'Spiderman', name: 'Peter Parker', country: 'US'}
 ];
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type

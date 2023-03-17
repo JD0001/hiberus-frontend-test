34135191af26422a16ca06683c003544707fa82a
Juande Delgado Bermúdez <25461819+JD0001@users.noreply.github.com>
2023-03-18 00:21:39 +0100

    Set ngx translate def. lang in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	public title ='hiberus-frontend-test';
+	public title = 'hiberus-frontend-test';
+
+	public constructor(private translate: TranslateService) {
+		//This avoids showing the ngx keys until the default language from translate.module.ts is loaded
+		translate.setDefaultLang('es');
+		translate.use('en');
+	}
 }

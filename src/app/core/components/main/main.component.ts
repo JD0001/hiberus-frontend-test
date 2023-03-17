77be0138426474282657ca052b93571db7c54fd4
Juande Delgado Bermúdez <25461819+JD0001@users.noreply.github.com>
2023-03-18 00:22:57 +0100

    Updated lang button to work with ngx-translate

--- src/app/core/components/main/main.component.ts
+++ src/app/core/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-main',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	public constructor(private translate: TranslateService) { }
+
+	/**
+	 * Provides an easy method for setting the locale from the HTML
+	 * @param locale language to show information in
+	 */
+	public setLocaleTo(locale: string): void {
+		this.translate.use(locale);
+	}
 }

f1ad7bcf54ac013cae2cdd0d71251ec8dd8809d1
Juande Delgado Bermúdez <25461819+JD0001@users.noreply.github.com>
2023-03-19 21:22:13 +0100

    Usage of cookies to keep language preference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
 	selector: 'app-root',
@@ -9,9 +10,15 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
 	public title = 'hiberus-frontend-test';
 
-	public constructor(private translate: TranslateService) {
-		//This avoids showing the ngx keys until the default language from translate.module.ts is loaded
-		translate.setDefaultLang('es');
-		translate.use('en');
+	public constructor(private translate: TranslateService, private cookies: CookieService) {
+
+		//Set the ngx-languages
+		translate.addLangs(['en', 'es']);
+
+		const fallbackLang = 'es';
+		translate.setDefaultLang(fallbackLang);
+
+		//Get language from cookies
+		translate.use(cookies.get('locale') || fallbackLang);
 	}
 }
